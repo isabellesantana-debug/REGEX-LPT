5869ae75bae7cfc7a5ef3d5fd19208b0424248e2
isabellesantana <isabell.santana@estudante.ifms.edu.br>
2026-09-23 10:40:41 -0400

    new file:   index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const email = "aluno@email.com";
+
+const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+if (regexEmail.test(email)) {
+  console.log("O email é válido.");
+} else {
+  console.log("O email é inválido.");
+}
